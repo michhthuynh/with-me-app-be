889414d144e155782852b8eda12a9ab4c617ac4c
ubuntu <Huynh Hoang>
2020-11-02 10:41:55 +0000

    add full name field into user model

--- src/controllers/Credentials/postLogin.controller.ts
+++ src/controllers/Credentials/postLogin.controller.ts
@@ -5,9 +5,15 @@ const postLogin = (req: Request, res: Response) => {
     const { username, password } = req.body
 
     sign({ username }, process.env.SECRET_STRING, { expiresIn: '24h' }, (err, token) => {
+        if(err) {
+            console.log(err.message)
+            res.sendStatus(503)
+            return
+        }
         res.json({
             token
         })
+        return
     })
 }
 
